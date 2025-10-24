1dc3d8c29647229b1d149778f6b4250224426f21
Hornet-silk <gustave.louis@proton.me>
2025-10-24 19:07:10 +0200

    Update bouton.js

--- bouton.js
+++ bouton.js
@@ -1,20 +1,9 @@
 const boutonfact = document.getElementById('boutonfact');
 const texteBlague = document.getElementById('texteBlague');
 
-const blagues = ["Unn, dans hollow knight,et Nuu, dans silksong, ont le même prénom mais inversé."
-                 "Le faux chevalier a volé l'armure de l'un des 5 grands chevaliers d'Hallownest",
-                 "Hornet est la demi-soeur du chevalier",
-                 "On peut détruire le masque du faiseur de masque,mais celui-ci en possède plusieurs",
-                 "Vous pouvez avoir les lucioles électriques dans votre journal du chasseur",
-                 "Cloth peut rester en vie, si vous affrontez le seigneur traitre seul",
-                 "Si vous ne tuez pas le forgeron d'aiguillon, celui-ci rejoindra Sheo et ils formeront un couple.",
-                 "L'endeuillé grise est l'un des 5 grands chevaliers d'Hallownest.",
-                 
-                 ];
+const blagues = ["Unn, dans hollow knight,et Nuu, dans silksong, ont le même prénom mais inversé.""Le faux chevalier a volé l'armure de l'un des 5 grands chevaliers d'Hallownest","Hornet est la demi-soeur du chevalier","On peut détruire le masque du faiseur de masque,mais celui-ci en possède plusieurs","Vous pouvez avoir les lucioles électriques dans votre journal du chasseur","Cloth peut rester en vie, si vous affrontez le seigneur traitre seul","Si vous ne tuez pas le forgeron d'aiguillon, celui-ci rejoindra Sheo et ils formeront un couple.","L'endeuillé grise est l'un des 5 grands chevaliers d'Hallownest.",];
 boutonfact.addEventListener('click', ()=>{
     console.log("testttt")
     texteBlague.textContent = blagues[Math.floor(Math.random()*blagues.length)];
 
 })
-
-
