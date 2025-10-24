80d57dcb563ee6ce3e6189709ce45c22e6c690a9
Hornet-silk <gustave.louis@proton.me>
2025-10-24 15:57:11 +0200

    Add files via upload

---
+++ club_hollow_knight/bouton.js
@@ -0,0 +1,8 @@
+const boutonfact = document.getElementById('boutonfact');
+const texteBlague = document.getElementById('texteBlague');
+
+const blagues = ["Le faux chevalier a volé l'armure de l'un des 5 grands chevaliers d'Hallownest", "Hornet est la demi-soeur du chevalier", "On peut détruire le masque du faiseur de masque, mais celui-ci en possède plusieurs","Vous pouvez avoir les lucioles électriques dans votre journal du chasseur","Cloth peut rester en vie, si vous affrontez le seigneur traitre seul","Si vous ne tuez pas le forgeron d'aiguillon, celui-ci rejoindra Sheo et ils formeront un couple.","L'endeuillé grise est l'un des 5 grands chevaliers d'Hallownest."];
+boutonfact.addEventListener('click', ()=>{
+    console.log("testttt")
+    texteBlague.textContent = blagues[Math.floor(Math.random()*blagues.length)];
+})
